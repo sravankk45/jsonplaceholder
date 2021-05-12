caf768786b1849c9f061dbe29beaa7a1f2a21b79
Sravan <sravank.587@gmail.com>
2021-05-13 01:02:12 +0530

    added folder structer and base test

---
+++ src/main/java/com/typicode/jsonplaceholder/base/TestBase.java
@@ -0,0 +1,33 @@
+package com.typicode.jsonplaceholder.base;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+
+public class TestBase{
+	
+	private static Properties properties;
+	protected String baseDir=System.getProperty("user.dir");
+	
+	public TestBase() {
+		
+		try {
+			properties=new Properties();
+			FileInputStream fileInputStream=new FileInputStream(baseDir+"/src/main/java/com/typicode/jsonplaceholder/config/config.properties");
+			properties.load(fileInputStream);
+		}
+		catch(IOException e) {
+		e.printStackTrace();
+		}
+		
+	}
+	
+	public String getBaseUri() {
+		String baseURI=properties.getProperty("baseURI");
+		return baseURI;
+	}
+	
+
+	
+}
